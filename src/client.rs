08508780fb9a2fcf590e34cabef1203ad057aef7
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-07-29 23:06:50 -0700

    Add methods onto client

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,28 @@
-pub struct Client {}
+use super::services;
+
+#[derive(Debug, Clone)]
+pub struct Client {
+    http: services::HttpService,
+}
+
+impl Client {
+    pub fn new(key: &str) -> Self {
+        let http = services::HttpService::new(key);
+
+        Self { http }
+    }
+
+    pub fn with_url(key: &str, url: &str) -> Self {
+        let http = services::HttpService::with_url(key, url);
+
+        Self { http }
+    }
+
+    pub fn set_key(&mut self, key: &str) {
+        self.http.set_key(key)
+    }
+
+    pub fn set_url(&mut self, url: &str) {
+        self.http.set_url(url)
+    }
+}

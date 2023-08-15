cbe2f2e9f197f619bb4c730deef4fdcf962a51ff
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-08-14 20:15:54 -0700

    Make msrv.sh dynamic and format cargo toml

--- scripts/msrv.sh
+++ scripts/msrv.sh
@@ -2,9 +2,11 @@
 
 MSRV=$(grep -oP '"msrv":"\K\d+\.\d+\.\d+' <<< $(cargo msrv --output-format json | tail -1))
 
-if [ "$MSRV" != "1.60.0" ]; then
+CURRENT_MSRV=$(sed -rn 's/"(.*)"/\1/p' <<< $(grep -oP 'rust-version = \K"(\d+\.\d+\.\d+)"' Cargo.toml))
+
+if [ "$MSRV" != "$CURRENT_MSRV" ]; then
   echo "MSRV changed!";
-  echo "Old: 1.60.0";
+  echo "Old: $CURRENT_MSRV";
   echo "New: $MSRV";
   exit 1;
 fi

45ed6cf767a25cadb4ebc51aca5321d0e48fe8eb
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-08-26 19:15:57 -0700

    Document models public module

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,9 @@
+//! This module houses the types you will be dealing with when sending and
+//! receiving requests from unkey.
+//!
+//! Mostly you will be constructing the structs suffixed with `Request`, and
+//! receiving the structs suffixed with `Response`. With some minor exceptions
+//! like [`Wrapped`] and [`UndefinedOr`].
 mod apis;
 mod http;
 mod keys;
